1af34ba448641fc762547338b19a98598f359cd4
Stephen <Stephen@HAL-9001>
2012-07-29 23:39:36 +0100

    It's alive!

--- src/com/miningcoop/blockchanger/BlockChanger.java
+++ src/com/miningcoop/blockchanger/BlockChanger.java
@@ -1,14 +1,37 @@
 package com.miningcoop.blockchanger;
 
+import java.util.HashSet;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BlockChanger extends JavaPlugin{
+public class BlockChanger extends JavaPlugin {
 	public void onEnable(){
-		getLogger().info("Plugin enabled, Bland is fucking awesome!");
+		getLogger().info("BlockChanger enabled.");
 	}
 	
 	public void onDisable(){
-		getLogger().info("aodhon probably broke something, didn't he?");
+		getLogger().info("BlockChanger disabled.");
 	}
 
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("asdf")) {
+			Player player = (Player)sender;
+			if(player.getGameMode().equals(GameMode.CREATIVE)) {
+				ItemStack is = player.getItemInHand();
+				Block b = player.getTargetBlock(new HashSet<Byte>(), 100);
+				b.setTypeId(is.getTypeId());
+				//This spawns a block where the players face is,
+				//suffocating the player.
+			}
+			return true;
+		}
+		return false;
+	}
 }
